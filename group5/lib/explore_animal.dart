51dc24d5202c88fd9591e0ed0174246d7a1b67ce
Steven Gunanto <Steven.Gunanto@rga.com>
2021-09-23 14:11:33 +0700

    Add explore and filter

---
+++ group5/lib/explore_animal.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:group5/filter_view.dart';
+
+class ExploreAnimal extends StatefulWidget {
+  ExploreAnimal({Key key}) : super(key: key);
+
+  @override
+  _ExploreAnimalState createState() => _ExploreAnimalState();
+}
+
+class _ExploreAnimalState extends State<ExploreAnimal> {
+  List a = [
+    "https://ca-times.brightspotcdn.com/dims4/default/35ea1cd/2147483647/strip/true/crop/4465x2976+0+0/resize/840x560!/format/webp/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F28%2Ffc%2F265139a5450da12db83ae1f3267b%2Fgettyimages-1126526494.jpg",
+    "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      backgroundColor: Color(0xffF2EEE5),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Explore"),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => FilterView()),
+                  );
+                },
+                child: Icon(
+                  Icons.filter_list,
+                  size: 26.0,
+                ),
+              )),
+        ],
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext ctx, int index) {
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: Card(
+              elevation: 20,
+              color: Color(0xff719065),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.network(a[index]),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.navigation_outlined,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          Text(
+                            "100k",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 18.0),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          "23 Sept 2021, 08:00",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "River Otters",
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "Family of river otters",
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: a.length,
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ group5/lib/filter_view.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class FilterView extends StatefulWidget {
+  const FilterView({Key key}) : super(key: key);
+
+  @override
+  _FilterViewState createState() => _FilterViewState();
+}
+
+class _FilterViewState extends State<FilterView> {
+  List<String> species = <String>["River Otters"];
+  String selectedSpecies = "River Otters";
+
+  List<String> times = <String>["6-12am", "1-6pm", "6-12pm"];
+  String selectedTime = "6-12am";
+
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Filter"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.check),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "SPECIES",
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            DropdownButton<String>(
+              items: species.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              value: selectedSpecies,
+              hint: Text("SELECT SPECIES"),
+              isExpanded: true,
+              onChanged: (selected) {
+                setState(() {
+                  selectedSpecies = selected;
+                });
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "LOCATION",
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            Slider(
+              value: _currentSliderValue,
+              min: 0,
+              max: 100,
+              divisions: 5,
+              label: _currentSliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _currentSliderValue = value;
+                });
+              },
+            ),
+            Center(
+              child: Text(
+                "$_currentSliderValue km from current location",
+                style: TextStyle(color: Colors.black, fontSize: 18.0),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "TIME",
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            DropdownButton<String>(
+              items: times.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              value: selectedTime,
+              hint: Text("SELECT TIME"),
+              isExpanded: true,
+              onChanged: (selected) {
+                setState(() {
+                  selectedTime = selected;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
